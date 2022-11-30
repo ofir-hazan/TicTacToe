c2d6bf3a7ae9f0f7b124ec4f45ba2abda7c0b776
ofir-hazan <322815093@idf.il>
2022-11-30 18:38:05 +0200

    Initial commit

---
+++ MyApplication/app/src/main/java/com/example/myapplication/StudetnListRow.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudetnListRow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studetn_list_row);
+    }
+}
